45425b0a544e0f477b7de5ea99edc24a5be7c70d
Shuyi Zhou <shuyizhou@Shuyis-MBP.fios-router.home>
2024-08-22 12:37:03 -0400

    added server

---
+++ server/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+let connectedClient;
+
+const DATABASE_NAME = 'Scholarship';
+const MONGODB_URI = "mongodb://localhost:27017/";
+
+
+
+export const connectClient = async () => {
+
+  
+  if (connectedClient) {
+    return connectedClient.db(DATABASE_NAME);
+  }
+
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  await client.db(DATABASE_NAME);
+  console.info("Connected to MongoDB");
+
+  connectedClient = client;
+
+  return connectedClient.db(DATABASE_NAME);
+};
+
+export const stopClient = async () => {
+  await connectedClient?.close();
+};
+
+  
+
+    
---
+++ server/index.js
@@ -0,0 +1,58 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { connectClient } from './db.js';
+
+const port = 3001;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+let db;
+const COLLECTION_NAME = 'data'
+connectClient().then(database => {
+  db = database;
+}).catch(err => {
+  console.error("Failed to connect to MongoDB", err);
+});
+
+
+app.get('/data',  async (req, res) => {
+    try {
+        const collection = db.collection(COLLECTION_NAME);
+        const response = await collection.find({});
+        console.log(response.data)
+        if (response) {
+            res.send(response.data);
+        } else {
+            res.status(404).send({});
+        }
+    } catch (error) {
+        console.error('Error retrieving data:', error);
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+});
+
+app.post('/data',  async(req, res) => {
+    const data = req.body;
+    
+    try {
+    const collection = db.collection(COLLECTION_NAME);
+        await collection.insertOne({data});
+    console.log('Received data:', data);
+    res.status(200).send({ message: 'Data received successfully' });
+    } catch (error) {
+        console.error('Error storing data:', error);
+        res.status(500).send({ message: 'Internal Server Error' });
+    }
+    
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
