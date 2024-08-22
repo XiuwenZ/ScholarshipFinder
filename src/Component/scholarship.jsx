94fd4bf1f33609abad9e2706167882f156d3de15
bradleyhung <bradleyhung5570@gmail.com>
2024-08-22 19:32:33 -0400

    mobile friendly + scrollbar

--- src/Component/scholarship.jsx
+++ src/Component/scholarship.jsx
@@ -4,18 +4,16 @@ import './scholarstyle.css';
 
 export default function Scholarship() {
   return (
-    <div className="container">
-        <div className="container-scholarship">
-            <Box className="Box">Scholarship 1</Box>
-            <Box className="Box">Scholarship 2</Box>
-            <Box className="Box">Scholarship 3</Box>
-            <Box className="Box">Scholarship 4</Box>
-            <Box className="Box">Scholarship 5</Box>
-            <Box className="Box">Scholarship 6</Box>
-            <Box className="Box">Scholarship 7</Box>
-            <Box className="Box">Scholarship 8</Box>
-            <Box className="Box">Scholarship 9</Box>
-        </div>
+    <div className="container-scholarship">
+        <Box className="Box">Scholarship 1</Box>
+        <Box className="Box">Scholarship 2</Box>
+        <Box className="Box">Scholarship 3</Box>
+        <Box className="Box">Scholarship 4</Box>
+        <Box className="Box">Scholarship 5</Box>
+        <Box className="Box">Scholarship 6</Box>
+        <Box className="Box">Scholarship 7</Box>
+        <Box className="Box">Scholarship 8</Box>
+        <Box className="Box">Scholarship 9</Box>
     </div>
   );
 }
