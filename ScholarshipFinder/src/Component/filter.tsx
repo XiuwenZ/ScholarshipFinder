19367182ab918b0f75e342266ec2238310a21567
bradleyhung <bradleyhung5570@gmail.com>
2024-08-23 17:06:51 -0400

    Hello World

--- ScholarshipFinder/src/Component/filter.tsx
+++ ScholarshipFinder/src/Component/filter.tsx
@@ -1,72 +1,76 @@
 import React from 'react'
 import './filter.css'
 
-const Checkbox = ({ label, value, onChange }) => {
+const Dropdown = ({ label, value, onChange, options }) => {
     return (
       <label>
-        <input type="checkbox" checked={value} onChange={onChange} />
         {label}
+        <select value={value} onChange={onChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
       </label>
     );
   };
 
 export default function Filter() {
-    const [checkedOne, setCheckedOne] = React.useState(false);
-    const [checkedTwo, setCheckedTwo] = React.useState(false);
-    const [checkedThree, setCheckedThree] = React.useState(false);
-    const [checkedFour, setCheckedFour] = React.useState(false);
-    const [checkedFive, setCheckedFive] = React.useState(false);
-    const [checkedSix, setCheckedSix] = React.useState(false);
-    const handleChangeOne = () => {
-        setCheckedOne(!checkedOne);
+    const [valueOne, setValueOne] = React.useState('');
+    const [valueTwo, setValueTwo] = React.useState('');
+    const [valueThree, setValueThree] = React.useState('');
+    const [valueFour, setValueFour] = React.useState('');
+    const [valueFive, setValueFive] = React.useState('');
+    const [valueSix, setValueSix] = React.useState('');
+
+    const handleChangeOne = (e) => {
+        setValueOne(e.target.value);
     }
-    const handleChangeTwo = () => {
-        setCheckedTwo(!checkedTwo);
+    const handleChangeTwo = (e) => {
+        setValueTwo(e.target.value);
     }
-    const handleChangeThree = () => {
-        setCheckedThree(!checkedThree);
+    const handleChangeThree = (e) => {
+        setValueThree(e.target.value);
     }
-    const handleChangeFour = () => {
-        setCheckedFour(!checkedFour);
+    const handleChangeFour = (e) => {
+        setValueFour(e.target.value);
     }
-    const handleChangeFive = () => {
-        setCheckedFive(!checkedFive);
+    const handleChangeFive = (e) => {
+        setValueFive(e.target.value);
     }
-    const handleChangeSix = () => {
-        setCheckedSix(!checkedSix);
+    const handleChangeSix = (e) => {
+        setValueSix(e.target.value);
     }
+
+    const options = ["Option 1", "Option 2", "Option 3"]; // Replace with your actual options
+
     return (
         <div className="container">
             <h2>Filters</h2>
             <ul className="container-body">
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 1" value={checkedOne} onChange={handleChangeOne} />
-                    <h3>Grades</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Grades" value={valueOne} onChange={handleChangeOne} options={options} />
                 </li>
 
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 2" value={checkedTwo} onChange={handleChangeTwo} />
-                    <h3>Location</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Location" value={valueTwo} onChange={handleChangeTwo} options={options} />
                 </li>
 
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 3" value={checkedThree} onChange={handleChangeThree} />
-                    <h3>Income</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Income" value={valueThree} onChange={handleChangeThree} options={options} />
                 </li>
 
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 4" value={checkedFour} onChange={handleChangeFour} />
-                    <h3>Jinfeng</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Jinfeng" value={valueFour} onChange={handleChangeFour} options={options} />
                 </li>
 
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 5" value={checkedFive} onChange={handleChangeFive} />
-                    <h3>Shuyi</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Shuyi" value={valueFive} onChange={handleChangeFive} options={options} />
                 </li>
 
-                <li className="CheckBoxOne">
-                    <Checkbox label="Value 6" value={checkedSix} onChange={handleChangeSix} />
-                    <h3>Xiuwen</h3>
+                <li className="DropdownOne">
+                    <Dropdown label="Xiuwen" value={valueSix} onChange={handleChangeSix} options={options} />
                 </li>
             </ul>
         </div>
