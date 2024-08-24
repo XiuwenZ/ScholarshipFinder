bd1be9597a21d43a01caca8514fd142bd3e6e10e
bradleyhung <bradleyhung5570@gmail.com>
2024-08-23 22:24:09 -0400

    Final_Product_For_realsies

--- ScholarshipFinder/src/App.tsx
+++ ScholarshipFinder/src/App.tsx
@@ -32,3 +32,41 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import Filter from './Component/filter.tsx'
+// import Scholarship from './Component/scholarship.tsx'
+// import FetchScholarshipsByGPA from './Component/fetch-scholarship-by-gpa.tsx'
+
+// interface Scholarship {
+//   name: string;
+//   income_range: string;
+//   race: string;
+//   awardAmount: string
+// }
+
+// function App() {
+//   const [scholarships, setScholarships] = React.useState<Scholarship[]>([]);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+
+//         <FetchScholarshipsByGPA setScholarships={setScholarships} />
+//         {scholarships.length > 0 ? (
+//           scholarships.map((scholarship) => (
+//             <div key={scholarship.name}>
+//                   <h1>{scholarship.name}</h1>
+//                   <h1>{scholarship.awardAmount}</h1>
+//           </div>
+//           ))
+//         ) : (
+//           <p>No scholarships found.</p>
+//         )}
+
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App
